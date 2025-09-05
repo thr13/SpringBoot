1ab59320d50be5959abaa30033780f9f3e2025f5
thr13 <skhrnt2945@gmail.com>
2025-09-05 17:23:28 +0900

    test: MemberRepository 기능 테스트

---
+++ src/test/java/com/boot/member/MemberRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.boot.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional //@Transactional 을 사용할려면 스프링 빈에 TransactionManager 가 있어야 한다
+    void memberTest() {
+        Member member = new Member("idA", "memberA");
+        memberRepository.initTable();
+        memberRepository.save(member);
+
+        Member findMember = memberRepository.find(member.getMemberId());
+        assertThat(findMember.getMemberId()).isEqualTo(member.getMemberId());
+        assertThat(findMember.getName()).isEqualTo(member.getName());
+    }
+}
