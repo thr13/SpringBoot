fee9f826b5be61062faeb0c1f7f45f2246a3f3ae
sugwan <skhrnt2945@gmail.com>
2025-09-09 12:22:45 +0900

    feat: @Value 를 이용한 외부 설정 주입

---
+++ src/main/java/com/boot/config/MyDataSourceValueConfig.java
@@ -0,0 +1,53 @@
+package com.boot.config;
+
+import com.boot.datasource.MyDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+import java.util.List;
+
+/**
+ * @Value("${외부 설정 변수}") 를 이용한 자동 외부 설정 주입
+ */
+@Slf4j
+@Configuration
+public class MyDataSourceValueConfig {
+
+    @Value("${my.datasource.url}")
+    private String url;
+
+    @Value("${my.datasource.username}")
+    private String username;
+
+    @Value("${my.datasource.password}")
+    private String password;
+
+    @Value("${my.datasource.etc.max-connection}")
+    private int maxConnection;
+
+    @Value("${my.datasource.etc.timeout}")
+    private Duration timeout;
+
+    @Value("${my.datasource.etc.options}")
+    private List<String> options;
+
+    @Bean
+    public MyDataSource myDataSource1() {
+        return new MyDataSource(url, username, password, maxConnection, timeout, options);
+    }
+
+    @Bean
+    public MyDataSource myDataSource2(
+            @Value("${my.datasource.url}") String url,
+            @Value("${my.datasource.username}") String username,
+            @Value("${my.datasource.password}") String password,
+            @Value("${my.datasource.etc.max-connection}") int maxConnection,
+            @Value("${my.datasource.etc.timeout}") Duration timeout,
+            @Value("${my.datasource.etc.options}") List<String> options) {
+
+        return new MyDataSource(url, username, password, maxConnection, timeout, options);
+    }
+}
