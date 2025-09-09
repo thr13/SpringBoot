13889ed35726da9305c5de2d8ec072e5e2856371
sugwan <skhrnt2945@gmail.com>
2025-09-09 22:28:11 +0900

    feat: @Profile 을 이용한 외부 설정 주입

--- src/main/java/com/boot/BootApplication.java
+++ src/main/java/com/boot/BootApplication.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Import;
 //@Import(MyDataSourceConfigV1.class)
 //@Import(MyDataSourceConfigV2.class)
 @Import(MyDataSourceConfigV3.class)
-@SpringBootApplication(scanBasePackages = "com.boot.datasource")
+@SpringBootApplication(scanBasePackages = {"com.boot.datasource", "com.boot.pay"})
 //@ConfigurationPropertiesScan({"com.boot"})
 public class BootApplication {
 
---
+++ src/main/java/com/boot/pay/PayClient.java
@@ -0,0 +1,5 @@
+package com.boot.pay;
+
+public interface PayClient {
+    void pay(int money);
+}
